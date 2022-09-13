26134e9c6251faaf96a197b0fe7461fec2b70ef9
kasperbc <s2000925@edu.bc.fi>
2022-09-13 13:34:10 +0300

    Initial commit

---
+++ Peliprojekti/Program.cs
@@ -0,0 +1,69 @@
+﻿using Peliprojekti;
+using System.Threading;
+
+Unit w1 = new Unit("Warrior 1", 10, 30);
+Unit w2 = new Unit("Warrior 2", 10, 30);
+
+Unit[] team1 = new Unit[] { w1 };
+Unit[] team2 = new Unit[] { w2 };
+
+Console.WriteLine("A battle is starting...");
+
+while (true)
+{
+    Attack(w1, w2);
+    if (CheckTeamsAlive(team1, team2) <= 1)
+    {
+        break;
+    }
+
+    Thread.Sleep(1000);
+
+    Attack(w2, w1);
+    if (CheckTeamsAlive(team1, team2) <= 1)
+    {
+        break;
+    }
+
+    Thread.Sleep(1000);
+}
+
+Console.WriteLine("Game over!");
+
+void Attack(Unit attacker, Unit victim)
+{
+    Console.WriteLine(attacker.GetName() + " attacks! " + victim.GetName() + " takes " + attacker.GetAttackPower() + " damage!");
+    victim.Damage(attacker.GetAttackPower());
+}
+
+int CheckTeamsAlive(Unit[] team1, Unit[] team2)
+{
+    int teamsAlive = 0;
+
+    if (GetTeamAliveCount(team1) > 0)
+    {
+        teamsAlive++;
+    }
+
+    if (GetTeamAliveCount(team2) > 0)
+    {
+        teamsAlive++;
+    }
+
+    return teamsAlive;
+}
+
+int GetTeamAliveCount(Unit[] t)
+{
+    int alive = 0;
+
+    foreach (Unit u in t)
+    {
+        if (u.GetStatus() != Unit.Status.Dead)
+        {
+            alive++;
+        }
+    }
+
+    return alive;
+}
---
+++ Peliprojekti/Unit.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peliprojekti
+{
+    internal class Unit
+    {
+        public enum Status { Neutral, Dead };
+
+        private Status status;
+        private string name;
+        private int attackPower;
+        private int hitPoints;
+
+        public Unit(string n, int a, int h)
+        {
+            name = n;
+            attackPower = a;
+            hitPoints = h;
+
+            status = Status.Neutral;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetAttackPower()
+        {
+            return attackPower;
+        }
+
+        public int GetHitPoints()
+        {
+            return hitPoints;
+        }
+
+        public Status GetStatus()
+        {
+            return status;
+        }
+
+        public void Damage(int dmg)
+        {
+            hitPoints -= dmg;
+
+            if (hitPoints < 0)
+            {
+                hitPoints = 0;
+                status = Status.Dead;
+            }
+        }
+    }
+}
